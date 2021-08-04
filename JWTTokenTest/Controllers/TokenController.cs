af6388faba575332eb789a2d056735c612b02b65
johnboytoo999 <johnboytoo999@gmail.com>
2021-08-05 01:34:18 +0800

    initial

---
+++ JWTTokenTest/Controllers/TokenController.cs
@@ -0,0 +1,69 @@
+﻿using JWTTokenTest.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTTokenTest.Controllers
+{
+    public class TokenController : ControllerBase
+    {
+        private readonly JwtHelpers jwt;
+
+        public TokenController(JwtHelpers jwt)
+        {
+            this.jwt = jwt;
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost("~/signin")]
+        public ActionResult<string> SignIn(LoginViewModel login)
+        {
+            if (ValidateUser(login))
+            {
+                return jwt.GenerateToken(login.Username);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private bool ValidateUser(LoginViewModel login)
+        {
+            return true; // TODO
+        }
+
+        [Authorize]
+        [HttpGet("~/claims")]
+        public IActionResult GetClaims()
+        {
+            return Ok(User.Claims.Select(p => new { p.Type, p.Value }));
+        }
+
+
+        [Authorize]
+        [HttpGet("~/username")]
+        public IActionResult GetUserName()
+        {
+            return Ok(User.Identity.Name);
+        }
+
+        [HttpGet("~/jwtid")]
+        public IActionResult GetUniqueId()
+        {
+            var jti = User.Claims.FirstOrDefault(p => p.Type == "jti");
+            return Ok(jti.Value);
+        }
+    }
+
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
+
